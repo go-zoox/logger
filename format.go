7054baa744ce4d5e1d4385872beb6de3665557c1
whatwewant <tobewhatwewant@outlook.com>
2023-02-23 09:31:23 +0800

    fix(transport): file transport better

--- format.go
+++ format.go
@@ -1,26 +1,26 @@
 package logger
 
-// Debug logs a debug message.
+// Debugf logs a debug message.
 func Debugf(format string, args ...interface{}) {
 	logger.Debug(format, args...)
 }
 
-// Info logs an info message.
+// Infof logs an info message.
 func Infof(format string, args ...interface{}) {
 	logger.Info(format, args...)
 }
 
-// Warn logs a warning message.
+// Warnf logs a warning message.
 func Warnf(format string, args ...interface{}) {
 	logger.Warn(format, args...)
 }
 
-// Error logs an error message.
+// Errorf logs an error message.
 func Errorf(format string, args ...interface{}) {
 	logger.Error(format, args...)
 }
 
-// Fatal logs a fatal message.
+// Fatalf logs a fatal message.
 func Fatalf(format string, args ...interface{}) {
 	logger.Fatal(format, args...)
 }
